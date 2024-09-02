ff3cf664c0099703808bfc53154cd42d6fd086ab
FREDGAR <fredy.huachaca21@gmail.com>
2024-09-01 21:49:16 -0500

    Implementación de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'product', component: ProductComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full'}, // Ruta por defecto
+  { path: '**', redirectTo: '/home', pathMatch: 'full' } // Ruta para páginas no encontradas
+];
